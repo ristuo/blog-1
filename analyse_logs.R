c325aab43821baa4b14ab06806e6b83885ae31aa
Risto Tuomainen <risto.tuomainen1@gmail.com>
2021-01-24 11:56:21 +0200

    Fix trivial bug

--- analyse_logs.R
+++ analyse_logs.R
@@ -6,6 +6,6 @@ article_reads <- df[article != ""]
 article_reads$datetime <- with_tz(as.POSIXct(article_reads$time, tz="GMT"), "Europe/Helsinki")
 article_reads$date <- as.Date(article_reads$datetime)
 reads_per_article <- article_reads[, .N, by = list(date, article)]
-print(reads_per_article[order("date")])
+print(reads_per_article[order(date)])
 
 
